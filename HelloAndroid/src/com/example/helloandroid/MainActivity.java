d525485562b697c6ea89a483a58f64b9f2aa72e3
Isacar Racine <irodrigu@harding.edu>
2014-02-05 16:02:57 -0600

    Added another log statement

--- HelloAndroid/src/com/example/helloandroid/MainActivity.java
+++ HelloAndroid/src/com/example/helloandroid/MainActivity.java
@@ -12,6 +12,7 @@ public class MainActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		Log.v("tag", "This is a test");
+		Log.v("tag", "This is another test");
 	}
 
 	@Override
