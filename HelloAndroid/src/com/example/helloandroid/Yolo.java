9aaf419ab4605d7025468da40c445fef18f71886
Isacar Racine <irodrigu@harding.edu>
2014-02-05 16:11:09 -0600

    Modified Yolo Class

--- HelloAndroid/src/com/example/helloandroid/Yolo.java
+++ HelloAndroid/src/com/example/helloandroid/Yolo.java
@@ -1,5 +1,24 @@
 package com.example.helloandroid;
 
 public class Yolo {
+	
+	Yolo(){
+		
+	
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public String getGender() {
+		return gender;
+	}
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+	private int age;
+	private String gender;
 
 }
